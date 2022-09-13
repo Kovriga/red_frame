80d69dbb18f139ed4c7c741d580ad36224f7ebc3
Kovriga <60592893+Kovriga@users.noreply.github.com>
2022-09-13 10:59:11 +0700

    #1 Создание навигатора

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,23 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+  <div id="app">
+    <nav-bar/>
+  </div>
 </template>
 
 <script lang="ts">
-import { Options, Vue } from 'vue-class-component';
-import HelloWorld from './components/HelloWorld.vue';
+import {Options, Vue} from 'vue-class-component';
+import NavBar from "@/components/NavBar.vue";
 
 @Options({
-  components: {
-    HelloWorld,
-  },
+  components: {NavBar}
 })
-export default class App extends Vue {}
+export default class App extends Vue {
+}
 </script>
 
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+body {
+  background-color: #F8F8FF;
+  overflow: hidden;
 }
 </style>
